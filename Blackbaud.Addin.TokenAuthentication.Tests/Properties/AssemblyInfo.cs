b2d135df6eee9395aaeecb90bfa779f7f39e6036
Ben Lambert <ben.lambert@blackbaud.com>
2017-10-13 10:07:05 -0400

    Initial checkin (#1)
    Initial checkin of the addin token authentication library

---
+++ Blackbaud.Addin.TokenAuthentication.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Blackbaud.Addin.TokenAuthentication.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Blackbaud.Addin.TokenAuthentication.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2dab1072-d300-4f83-8f53-f7c5a1393748")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Blackbaud.Addin.TokenAuthentication/Exceptions/InvalidTokenSignatureException.cs
@@ -0,0 +1,23 @@
+﻿namespace Blackbaud.Addin.TokenAuthentication.Exceptions
+{
+    /// <summary>
+    /// Definition for InvalidTokenSignatureException
+    /// </summary>
+    public class InvalidTokenSignatureException : TokenValidationException
+    {
+        /// <summary>
+        /// Initializes a new instance of the InvalidTokenSignatureException class.
+        /// </summary>
+        public InvalidTokenSignatureException() : base()
+        {
+        }
+        
+        /// <summary>
+        /// Initializes a new instance of the InvalidTokenSignatureException class.
+        /// </summary>
+        /// <param name="message">message describing that the signature of the token is invalid.</param>
+        public InvalidTokenSignatureException(string message) : base(message)
+        {
+        }
+    }
+}
